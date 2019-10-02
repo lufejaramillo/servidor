53956146a2c655d4b2737f1c5d642716cf5ec680
dortize <diegoantonio.e@gmail.com>
2019-10-02 20:55:52 -0300

    Comentarios modificados

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import time
 
 # Servidor
 
-HOST = '192.168.1.6'  # Dirección IP o Hostname del servidor
+HOST = '192.168.1.6'  # Dirección IP o hostname del servidor
 PORT = 65432        # Puerto de escucha (puertos no privilegiados > 1024)
 
 Hora = time.strftime("%H:%M:%S;")
