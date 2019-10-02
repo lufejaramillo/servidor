ac1248dc3bc0595267ab37a307a7f98cfab53ac9
lufejaramillo <luis.jaramillo.godoy.14@gmail.com>
2019-10-02 19:18:23 -0300

    comentario esp

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import time
 
 HOST = '192.168.1.6'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 65432        # puerto de escucha (puertos no privilegiados > 1024)
 Hora = time.strftime("%H:%M:%S;")
 Fecha = time.strftime("%Y/%m/%d")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
